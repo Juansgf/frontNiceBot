10b0cbf5002f13369072943ce6652125858bae51
Juan <jesoberanis@gmail.com>
2020-10-28 18:00:36 -0600

    vistas

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,12 +3,18 @@ import Navbar from './Navbar';
 
 
 const Tweet = () => {
+
+
+
+
+
+
   return (
   <div> 
     <Navbar/>
     <header className="text-white text-center" style={{height: "409px"}}>
         <div className="container text-center" >
-            <h1 className="font-weight-bold" style={{textAlign: "center"}}>Tweet Example</h1>
+            <h1 className="font-weight-bold" style={{textAlign: "center",margin:"5%"}}>¿qué hacer para que a una mujer le brillen los ojos? ponerle una linterna en la oreja. #sincerebro #sinderechos #sinpolla</h1>
             <hr className="star-light"/>
              
         </div>
@@ -17,10 +23,10 @@ const Tweet = () => {
         <div style={{paddingBottom: "5px"}}>
             <h3 className="text-dark mb-0 d-inline-block">Vote</h3>
         </div>
-        <div className="row">
-            <div class="col-md-4"><button class="color-button" type="button">Button</button></div>
-            <div class="col-md-4"><button class="middle-button" type="button">Button</button></div>
-            <div class="col-md-4"><button class="last-button" type="button">Button</button></div>
+        <div className="row" style={{paddingTop: "13px"}} >
+            <div class="col-md-4"><button class="color-button" type="button">Misogino</button></div>
+            <div class="col-md-4"><button class="middle-button" type="button">Otro Tweet</button></div>
+            <div class="col-md-4"><button class="last-button" type="button">No Misognio</button></div>
         </div>
     </div>
     
